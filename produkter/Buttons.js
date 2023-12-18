269d1ce1835c9ce3074bfc48d70023914dba8411
TrymCoder <trymel.koder@gmail.com>
2023-12-18 13:39:11 +0100

    first commit

---
+++ produkter/Buttons.js
@@ -0,0 +1,5 @@
+  // JavaScript function to redirect to the product page based on the clicked button
+  function redirectToProduct(productId) {
+    // Replace the following URL with the actual URL structure of your product pages
+    window.location.href = '/products/' + productId;
+}
